7e19f6d8b3a34ec179a5836d8bfc2b0567c84a0e
cdsalty <soltiscd@gmail.com>
2019-04-29 23:54:42 -0400

    mapping through, rendering pokethings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import Pokecard from './Pokecard';
+//import Pokecard from './Pokecard'; // replaced by Pokedex, the higher level component.
+import Pokedex from './Pokedex';
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Pokecard id={4} name="Charmander" type="fire" exp={62} />
+        {/* replaced Pokecard below with a plain <Pokedex />
+        <Pokecard id={4} name="Charmander" type="fire" exp={62} /> */}
+        <Pokedex />
       </div>
     );
   }
