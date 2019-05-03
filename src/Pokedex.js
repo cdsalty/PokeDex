9fb0be498cb67d0d1b60c4fc781b63da0cd36d5d
cdsalty <soltiscd@gmail.com>
2019-05-03 16:26:38 -0400

    determines winning and losing hand

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -22,7 +22,7 @@ class Pokedex extends Component {
             <div className="Pokedex">
               <h1>Pokedex!!!</h1>
               <p>Total Experience: {this.props.exp}</p>
-              <p>{this.props.isWinner}</p>
+              <p>{this.props.isWinner ? 'Winner' : 'Loser'}</p>
               <div className = "Pokedex-cards">
                 {this.props.pokemonKey.map((p) => (
                     // render a Pokecard by mapping over and producing a card with the id, name, type and experience
